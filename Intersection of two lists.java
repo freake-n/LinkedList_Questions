ef2db30e4e568de1bdde160de499143b68cf4df7
Sabuj Dutta <112894964+freake-n@users.noreply.github.com>
2024-03-24 21:59:07 +0530

    Create Intersection of two lists.java

---
+++ Intersection of two lists.java
@@ -0,0 +1,59 @@
+/**
+
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+
+Example 1:
+
+
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+Output: Intersected at '8'
+Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+- Note that the intersected node's value is not 1 because the nodes with value 1 in A and B (2nd node in A and 3rd node in B) are different node references. In other words, they point to two different locations in memory, while the nodes with value 8 in A and B (3rd node in A and 4th node in B) point to the same location in memory.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA=headA;
+        ListNode tempB=headB;
+        int ca=0,cb=0;
+        while(tempA!=null){
+            ca++;
+            tempA=tempA.next;
+        }
+        tempA=headA;
+        while(tempB!=null){
+            cb++;
+            tempB=tempB.next;
+        }
+        tempB=headB;
+        if(ca>cb){
+            for(int i=1;i<=(ca-cb);i++){
+                tempA=tempA.next;
+            }
+        }
+        else{
+            for(int i=1;i<=(cb-ca);i++){
+                tempB=tempB.next;
+            }
+        }
+        while(tempA!=tempB){
+            tempA=tempA.next;
+            tempB=tempB.next;
+        }
+        return tempA;
+    }
+}
