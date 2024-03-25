570bb7ff0170ce5addc236bef9b3990c605582bb
Sabuj Dutta <112894964+freake-n@users.noreply.github.com>
2024-03-25 07:51:32 +0530

    Create Middle of list.java

---
+++ Middle of list.java
@@ -0,0 +1,38 @@
+/**
+
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){   // 1st cond. -> Even no. of nodes (for Right Middle)    2nd cond. -> Odd no. of nodes
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        return slow;
+    }
+}
